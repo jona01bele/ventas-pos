c3e9aa760919bba6e01ead7ef62850713b7021c2
jona01bele <127359203+jona01bele@users.noreply.github.com>
2024-05-30 08:09:08 -0500

    probando los cambio nuevos

--- resources/views/comun/buscador.blade.php
+++ resources/views/comun/buscador.blade.php
@@ -1,11 +1,15 @@
+<!--Buscador para utilizar en todos los componentes de nuestro proyecto-->
 <div class="row justify-content-between">
     <!--para resoluciones largar 4 columnas , medianas 4 y pequeñas 12-->
     <div class="col-lg-4 col-md-4 col-sm-12">
-        <div class="input-group-prepend">
-           <span class="input-group-text inuput-gp">
-                <i class="fas fa-seach"></i>
-            </span> 
-            <input type="text" wire:modal="buscar" class="form-control">
+        <div class="input-group mb-4">
+            <div class="input-group-prepend">
+                <span class="input-group-text inuput-gp">
+                     <i class="fas fa-seach"></i>
+                 </span>     
+             </div>
+             <input type="text" wire:modal="buscar" placeholder="Buscar" class="form-control">
         </div>
+        
     </div>
 </div>
